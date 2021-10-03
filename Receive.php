fa2c91452dd1f68caab42f460581ea0e8ea1a068
Rafael Oliveira <rafaeloliveira0019@hotmail.com>
2021-10-03 12:48:57 -0300

    Criado producer/consumer rabbitmq

---
+++ Receive.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection("fly.rmq.cloudamqp.com", 5672, 'kczkzyms', 'te7SOSx6c91t4uZ_X4megwV_tpJGLVIR', "kczkzyms");
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+$callback = function ($msg) {
+    echo ' [x] Received ', $msg->body, "\n";
+};
+
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while ($channel->is_open()) {
+    $channel->wait();
+}
